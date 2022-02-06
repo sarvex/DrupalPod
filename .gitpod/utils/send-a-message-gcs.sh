a0a9def6c391ee3815923b7519f0b664826d408d
Ofer Shaal <shaalofer@gmail.com>
2022-02-06 06:17:26 +0000

    remove last if

--- .gitpod/utils/send-a-message-gcs.sh
+++ .gitpod/utils/send-a-message-gcs.sh
@@ -22,11 +22,11 @@ fi
 mc config host add gcs https://storage.googleapis.com "$DP_GOOGLE_ACCESS_KEY" "$DP_GOOGLE_SECRET"
 
 # If there's a problem send the error code
-if mc find gcs/drupalpod/ready-made-envs.tar.gz; then
-    message="Success: Google Cloud is ready"
-else
-    message="Error: Envs file wasn't found, it will be recreated"
-fi
+# if mc find gcs/drupalpod/ready-made-envs.tar.gz; then
+#     message="Success: Google Cloud is ready"
+# else
+#     message="Error: Envs file wasn't found, it will be recreated"
+# fi
 
 # Send a message through IFTTT
 # curl -X POST -H "Content-Type: application/json" -d "{\"value1\":\"$branch_user\",\"value2\":\"$message\"}" https://maker.ifttt.com/trigger/drupalpod_prebuild_initiated/with/key/"$IFTTT_TOKEN"
